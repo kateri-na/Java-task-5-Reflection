704b20bc74c8ebd89d68f68d38d936d0a758c780
katerina <katerina_panina1@mail.ru>
2024-04-17 12:43:56 +0300

    add Injector class

---
+++ src/main/java/Injector.java
@@ -0,0 +1,33 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.text.Annotation;
+import java.util.Properties;
+
+public class Injector<T> {
+    private Properties properties;
+    Injector(String filePath) throws IOException {
+        properties = new Properties();
+        properties.load(new FileReader(filePath));
+    }
+
+    T inject(T object) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+        Class dependency;
+        Class gettenClass = object.getClass();
+        Field[] fields = gettenClass.getDeclaredFields();
+        for(Field field: fields){
+            if(field.isAnnotationPresent(AutoInjectable.class)){
+                Class fieldType = field.getType();
+                String interfaceName = fieldType.getName();
+                String implementationClassName = properties.getProperty(interfaceName);
+                if (implementationClassName != null) {
+                    dependency = Class.forName(implementationClassName);
+                    field.setAccessible(true);
+                    field.set(object, dependency.newInstance());
+                }
+            }
+        }
+        return object;
+    }
+}
